bcd9cd86c2cecf33f40f5da60dcae900346086d8
Aliszson <aligbr150@gmail.com>
2025-06-24 11:39:21 -0300

    retorno correto do usuario

--- src/breeze/settings.py
+++ src/breeze/settings.py
@@ -98,6 +98,12 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 10,
 }
 
+DJOSER = {
+    'SERIALIZERS': {
+        'current_user': 'api_bb.serializers.UsuarioSerializer',
+    }
+}
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
